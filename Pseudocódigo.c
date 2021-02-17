e622bbb2b6a8b6c007dcb0ff9f851a5f5396ad08
vprrkmhsy <vprrkmhsy@gmail.com>
2021-02-17 15:48:28 -0300

    ver 1.2 - Tratamento de interrupcao do Timer adicionado, interrupcao dos botoes e do timer habilitada e comandos 00, 01 e 02 implementados;

--- Pseudocódigo.c
+++ Pseudocódigo.c
@@ -2,27 +2,44 @@
 
 /* Definicao dos Enderecos Necessarios */
 #define STACK		0x10000
-#define BASE_IO		0x10000000
+#define BASE_IO 	0x10000000
 // offsets
-#define RED_LED		0x0
-#define DISPLAY		0x20
+#define RED_LED 	0x0
+#define DISPLAY 	0x20
 #define SWITCHES	0x40
-#define BUTTONS		0x50
+#define BUTTONS 	0x50
+#define BTN_Mask	0x58
+#define BTN_Edge	0x5C
 #define JTAG_Data	0x1000
 #define JTAG_Ctrl	0x1004
-#define TIMER		0x2000
+#define TM_Status	0x2000
+#define TM_Ctrl 	0x2004
+#define TM_Vle_Low	0x2008
+#define TM_Vle_High 0x200C
 
 /* Definicao dos Metodos chamados por Interrupcao */
 // Possivel que algum metodo seja mudado para polling no codigo principal
 
+// variáveis de uso geral
 int buffer[5]; // buffer vai atualizando ate usuario dar enter, no enter chama COMANDO_VALIDADO();
 int lastChar;
+bool ledOn = false;
+int leds[18];
 // Tratamento de Interrupcao
 void RTI(int dispositivo){
 	switch(dispositivo){
 		case TIMER:
+			if(ledOn){
+				stwio( r0, RED_LED*r8 );
+				ledOn = false;
+			}else{
+				lwd( rYY, leds ); // movia rXY, leds; ldw rYY, (rXY)
+				stwio( rYY, RED_LED*r8 );
+				ledOn = true;
+			}
 			break;
 		case BUTTONS:
+			
 			break;
 		case JTAG_Ctrl:
 			if(lastChar == 0x0a /* ENTER */ ){
@@ -34,7 +51,7 @@ void RTI(int dispositivo){
 				ldwio( rXX, JTAG_Data*r8 ); // le data (reseta RI e pega char)
 				// exemplo: buffer = 0xaabbccddee, rXX = 0xff
 				rYY = buffer; // rYY = 0xaabbccddee;
-				shitfLeft( rYY, rYY, 8 ); // 0xbbccddee00;
+				slli( rYY, rYY, 8 ); // 0xbbccddee00;
 				or( buffer, rYY, rXX ); // 0xbbccddeeff;
 				
 				stwio( JTAG_Data*r8, rXX); // escreve char no terminal
@@ -74,15 +91,28 @@ void COMANDO_VALIDADO(){
 }
 // Ativa pulsacao do xx-ésimo LED vermelho
 void COMANDO_00(int xx){
-	
+	// leds[xx] = 1;
+	rXX = 0b1
+	slli( rXX, rXX, xx );
+	lwd( rYY, leds ); // movia rXY, leds; ldw rYY, (rXY)
+	or( rYY, rYY, rXX );
+	stw( rYY, leds );
 }
 // Desativa pulsacao do xx-ésimo LED vermelho
 void COMANDO_01(int xx){
-	
+	// leds[xx] = 0;
+	rXX = 0xFFFFFFFE // movhi 0xFFFF; ori 0xFFFE
+	roli( rXX, rXX, xx );
+	lwd( rYY, leds ); // movia rXY, leds; ldw rYY, (rXY)
+	and( rYY, rYY, rXX );
+	stw( rYY, leds );
 }
 // Destaiva pulsacao de todos os LED vermelhos
 void COMANDO_02(){
-	
+	// lenOn = 0;
+	lwd( rYY, leds ); // movia rXY, leds; ldw rYY, (rXY)
+	and( rYY, rYY, r0 );
+	stw( rYY, leds );
 }
 // Mostra numero triangular das chaves nos Displays de 7-seg
 void COMANDO_10(){
@@ -112,6 +142,16 @@ int main(){
 	r8 = BASE_IO;
 	
 	/* Habilitacao das interrupcoes necessarias */
+	// Timer
+	r10 = 0x7;
+	stwio( r10, TM_Ctrl*r8 );
+	r10 = 0xFFFF // valor provisorio
+	stwio( r10, TM_Vle_Low*r8 );
+	r10 = 0xF // valor provisorio
+	stwio( r10, TM_Vle_High*r8 );
+	// Botoes
+	r10 = 0x6;
+	stwio( r10, BTN_MASK*r8 );
 	// JTAG
 	ldwio( r10, JTAG_Ctrl*r8 );
 	ori( r10, r10, 0x1 );
