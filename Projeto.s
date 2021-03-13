63f6868bb38724fdac3d20366d74e5354027c401
vprrkmhsy <vprrkmhsy@gmail.com>
2021-03-13 18:56:50 -0300

    ver 2.4  - Mensagem de erro ao entrar com comando inexistente implementada  - Código melhor comentado

--- Projeto.s
+++ Projeto.s
@@ -2,7 +2,6 @@
 .equ	BASE_IO,	0x10000000
 # offsets
 .equ	RED_LED,	0x0
-.equ	GREEN_LED,	0x10 # proposito de teste
 .equ	D7S_Low,	0x20
 .equ	D7S_High,	0x30
 .equ	SWTCH,		0x40
@@ -25,32 +24,32 @@
 	stw 	r5, 4(sp)
 	stw 	ra, (sp)
 	
-	# Verifica de onde veio a interrupcao
+	# Verifica de onde veio a excecao
 	rdctl	et, ipending
-	beq 	et, r0, OTHER_EXC
-	br  	INTERRUPT
+	beq 	et, r0, OTHER_EXC # Caso nao seja interrupcao
+	br  	INTERRUPT # Caso seja interrupcao
 	
-	OTHER_EXC:
+	OTHER_EXC: # Para excecoes que nao sejam interrupcoes
 	br  	QUIT_RTI
 	
 	INTERRUPT:
 	subi	ea, ea, 0x4
-	
+	# Verifica qual dispositivo gerou a interrupcao
 	andi	r11, et, 0x100
 	beq 	r11, r0, NOT_JTAG
-	call	JTAG_INTERRUPT
+	call	JTAG_INTERRUPT # Chama o tratamento para JTAG
 	br  	QUIT_RTI
 	NOT_JTAG:
 	
 	andi	r11, et, 0x2
 	beq 	r11, r0, NOT_BUTT
-	call	BUTT_INTERRUPT
+	call	BUTT_INTERRUPT # Chama o tratamento para os botoes
 	br  	QUIT_RTI
 	NOT_BUTT:
 	
 	andi	r11, et, 0x1
 	beq 	r11, r0, NOT_TIMER
-	call	TIMER_INTERRUPT
+	call	TIMER_INTERRUPT # Chama o tratamento para o timer
 	br  	QUIT_RTI
 	NOT_TIMER:
 	
@@ -66,29 +65,29 @@
 	addi	sp, sp, 32
 	eret
 
-TIMER_INTERRUPT:
+TIMER_INTERRUPT: # Tratamento para interrupcao do timer
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
-	# Acende/apaga os leds
+	# Acende/apaga os leds vermelhos
 	ldwio	r11, RED_LED(r8)
-	beq 	r11, r0, TURN_ON
-	br		TURN_OFF
+	beq 	r11, r0, TURN_ON # Caso valor atual == 0 (apagado), acender
+	br		TURN_OFF # Caso != 0 (aceso), apagar
 	TURN_ON:
-	movia 	r12, LED_SEQ
-	ldw 	r11, 0(r12)
-	stwio	r11, RED_LED(r8)
+	movia 	r12, LED_SEQ # Pega o endereco da sequencia de leds
+	ldw 	r11, 0(r12) # Pega o valor da sequencia de leds
+	stwio	r11, RED_LED(r8) # Acende de acordo com o valor da sequencia de leds
 	br		END_TURN
 	TURN_OFF:
-	stwio	r0, RED_LED(r8)
+	stwio	r0, RED_LED(r8) # Apaga
 	END_TURN:
-	stwio	r0,	Timer(r8)
+	stwio	r0,	Timer(r8) # Reseta RUN e TO
 	
 	ldw 	ra, (sp)
 	addi	sp, sp, 4
 	ret
 	
-BUTT_INTERRUPT:
+BUTT_INTERRUPT: # Tratamento para interrupcao dos botoes
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
@@ -122,18 +121,17 @@ BUTT_INTERRUPT:
 	OTHER_BUTT:
 	# Verifica se rotacao esta pausada
 	ldb 	r13, 1(r9)
-	bne 	r13, r0, ROT_ON
-	# Se estiver pausada, retoma
+	bne 	r13, r0, ROT_ON # Se != 0 (ativa), pausa
+	# Se = 0 (pausada), retoma
 	ldb 	r13, 2(r9)
 	stb 	r13, 1(r9)
 	br  	QUIT_BUTT
-	# Se nao, pausa
 	ROT_ON:
 	stb 	r13, 2(r9)
 	stb 	r0, 1(r9)
 	
 	QUIT_BUTT:
-	
+	# Reseta a captura de borda para a proxima interrupcao
 	movia	r11, 0x6
 	stwio	r11, PB_Edge(r8)
 	
@@ -141,49 +139,47 @@ BUTT_INTERRUPT:
 	addi	sp, sp, 4
 	ret
 	
-JTAG_INTERRUPT:
+JTAG_INTERRUPT: # Tratamento para interrupcao do JTAG
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
 	ldwio	r13, JTAG_Data(r8)
 	
-	movi	r11, 0x0a
-	andi	r12, r13, 0xFF
-	beq 	r11, r12, ENTER_INPUTED
-	
-	# atualiza o buffer do comando com o novo char
+	movi	r11, 0x0a # Valor do caracter <ENTER>
+	andi	r12, r13, 0xFF # Pega caracter mais recente na fila
+	beq 	r11, r12, ENTER_INPUTED # Verifica se <ENTER> foi pressionado
+	# Caso <ENTER> nao pressionado
+	/* Atualiza o buffer de comando com o novo caracter */
+	# 01234 -> 1234[]
 	ldb 	r11, 1(r10)
 	stb 	r11, 0(r10)
-	
 	ldb 	r11, 2(r10)
 	stb 	r11, 1(r10)
-	
 	ldb 	r11, 3(r10)
 	stb 	r11, 2(r10)
-	
 	ldb 	r11, 4(r10)
 	stb 	r11, 3(r10)
-	
+	# Com a posicao 4 "vazia", salva o novo caracter na mesma
 	stb 	r13, 4(r10)
 	
 	CHAR_INPUTED:
-	stwio	r13, JTAG_Data(r8)
+	stwio	r13, JTAG_Data(r8) # Imprime o caracter no terminal
 	br		INPUT_END
 	
-	ENTER_INPUTED:
-	call	VALID_COMMAND
+	ENTER_INPUTED: # Caso <ENTER> pressionado
+	call	VALID_COMMAND # Chama interpretador de comando
 	
-	# limpa buffer
+	# Limpa buffer de comando
 	stb 	r0, 0(r10)
 	stb 	r0, 1(r10)
 	stb 	r0, 2(r10)
 	stb 	r0, 3(r10)
 	stb 	r0, 4(r10)
 	
-	# imprime o caracter lido no terminal
+	# Pula linha (imprime <ENTER> no terminal)
 	stwio	r13, JTAG_Data(r8)
 	
-	# imprime "cmd:"  no terminal
+	# Imprime "cmd:" no terminal
 	mov 	r11, r0
 	movi	r11, 0x63
 	stwio	r11, JTAG_Data(r8)
@@ -200,19 +196,19 @@ JTAG_INTERRUPT:
 	addi	sp, sp, 4
 	ret
 
-VALID_COMMAND:
+VALID_COMMAND: # Interpretador de comando
 	addi	sp, sp, -16
 	stw 	r13, 12(sp)
 	stw 	r12, 8(sp)
 	stw 	r11, 4(sp)
 	stw 	ra, (sp)
 	
-	# interpretador do comando
 	COMMAND_CHECK:
-	ldb 	r11, 2(r10) # verifica se tem "espaco" nessa posicao
-	movi	r12, 0x20
-	beq 	r11, r12, HAS_SPACE
-	NO_SPACE: # se nao tiver, eh um comando simples
+	ldb 	r11, 2(r10) # Pega caracter na posico 2
+	movi	r12, 0x20 # Valor do caracter <SPACE>
+	beq 	r11, r12, HAS_SPACE # Verifica se é <SPACE>
+	NO_SPACE: # Caso nao tenha espaco, trata-se de um comando simples (sem passagem de parametro)
+	# Verifica o primeiro digito do comando
 	ldb 	r11, 3(r10)
 	movi	r12, 0x30
 	beq 	r11, r12, SIMPLE_0
@@ -222,7 +218,8 @@ VALID_COMMAND:
 	beq 	r11, r12, SIMPLE_2
 	br  	NOT_FOUND
 	
-		SIMPLE_0:
+	/* Verifica, dependendo do primeiro digito, o segundo digito do comando */
+		SIMPLE_0: # Para digito inicial 0
 	ldb 	r11, 4(r10)
 	movi	r12, 0x32
 	beq 	r11, r12, CMD_02
@@ -230,13 +227,13 @@ VALID_COMMAND:
 	beq 	r11, r12, CMD_03
 	br		NOT_FOUND
 	
-		SIMPLE_1:
+		SIMPLE_1: # Para digito inicial 1
 	ldb 	r11, 4(r10)
 	movi	r12, 0x30
 	beq 	r11, r12, CMD_10
 	br		NOT_FOUND
 	
-		SIMPLE_2:
+		SIMPLE_2: # Para digito inicial 2
 	ldb 	r11, 4(r10)
 	movi	r12, 0x30
 	beq 	r11, r12, CMD_20
@@ -244,13 +241,15 @@ VALID_COMMAND:
 	beq 	r11, r12, CMD_21
 	br  	NOT_FOUND
 	
-	HAS_SPACE: # se tem, eh um comando composto (com parametro passado)
+	HAS_SPACE: # Caso tenha espaco, trata-se de um comando com passagem de parametro
+	# Verifica o primeiro digito do comando
 	ldb 	r11, 0(r10)
 	movi	r12, 0x30
 	beq 	r11, r12, CMPLX_0
 	br  	NOT_FOUND
 	
-		CMPLX_0:
+	/* Verifica, dependendo do primeiro digito, o segundo digito do comando */
+		CMPLX_0: # Para digito inicial 0
 	ldb 	r11, 1(r10)
 	movi	r12, 0x30
 	beq 	r11, r12, CMD_00
@@ -258,7 +257,7 @@ VALID_COMMAND:
 	beq 	r11, r12, CMD_01
 	br		NOT_FOUND
 	
-	# tabela de comandos
+	# Tabela de comandos existentes
 		CMD_00:
 	call	LED_ON
 	br		END_CHECK
@@ -281,8 +280,47 @@ VALID_COMMAND:
 	call	WORD_OFF
 	br		END_CHECK
 	
-	NOT_FOUND:
-	# talvez imprima algo no jtag
+	NOT_FOUND: # Caso comando nao seja encontrado, imprime mensagem de erro
+	mov 	r11, r0
+	movi	r11, 0x0a
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x43
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6F
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6D
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6D
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x61
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6E
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x64
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x20
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6E
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6F
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x74
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x20
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x66
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6F
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x75
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x6E
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x64
+	stwio	r11, JTAG_Data(r8)
+	movi	r11, 0x21
+	stwio	r11, JTAG_Data(r8)
+	
 	END_CHECK:
 	
 	ldw 	r11, 4(sp)
@@ -296,23 +334,24 @@ LED_ON: # comando 00
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
-	# identifica qual o led escolhido
+	# Identifica qual o led escolhido
 	ldb 	r11, 3(r10)
-	subi	r11, r11, 0x30 # normaliza valor de char pra int
+	subi	r11, r11, 0x30 # Normaliza valor de char pra int
 	muli	r13, r11, 0xa
 	ldb 	r11, 4(r10)
-	subi	r11, r11, 0x30 # normaliza valor de char pra int
+	subi	r11, r11, 0x30 # Normaliza valor de char pra int
 	add 	r13, r13, r11
-	
-	beq 	r13, r0, END_ON # caso o valor seja o led 00, nao encontra o led, ja que comeca em 01, e finaliza
+	/* Para valores fora de alcance */
+	beq 	r13, r0, END_ON # Caso o valor seja o led 00, nao encontra o led, ja que comeca em 01, e finaliza
 	movi	r11, 0x21
-	bge 	r13, r11, END_ON # caso o valor seja maior que 32, nao encontra o led, ja que termina em 32, e finaliza
+	bge 	r13, r11, END_ON # Caso o valor seja maior que 32, nao encontra o led, ja que termina em 32, e finaliza
 	
+	# Coloca 1 na posicao do led
 	subi	r13, r13, 0x1
 	movi	r11, 0x1
 	sll 	r11, r11, r13
 	
-	# acende o led escolhido
+	# Acende o led escolhido (ativa o bit correspondente na sequencia de leds)
 	movia 	r12, LED_SEQ
 	ldw 	r13, 0(r12)
 	or		r13, r13, r11
@@ -328,27 +367,29 @@ LED_OFF: # comando 01
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
-	# identifica qual o led escolhido
+	# Identifica qual o led escolhido
 	ldb 	r11, 3(r10)
-	subi	r11, r11, 0x30 # normaliza valor de char pra int
+	subi	r11, r11, 0x30 # Normaliza valor de char pra int
 	muli	r13, r11, 0xa
 	ldb 	r11, 4(r10)
-	subi	r11, r11, 0x30 # normaliza valor de char pra int
+	subi	r11, r11, 0x30 # Normaliza valor de char pra int
 	add 	r13, r13, r11
-	
-	beq 	r13, r0, END_OFF # caso o valor seja o led 0, nao encontra o led, ja que comeca em 1, e finaliza
+	/* Para valores fora de alcance */
+	beq 	r13, r0, END_OFF # Caso o valor seja o led 0, nao encontra o led, ja que comeca em 1, e finaliza
 	movi	r11, 0x21
-	bge 	r13, r11, END_OFF # caso o valor seja maior que 32, nao encontra o led, ja que termina em 32, e finaliza
+	bge 	r13, r11, END_OFF # Caso o valor seja maior que 32, nao encontra o led, ja que termina em 32, e finaliza
 	
+	# Coloca 1 na posicao do led
 	subi	r13, r13, 0x1
 	movi	r11, 0x1
 	sll 	r11, r11, r13
 	
+	# Cria a sequencia inversa (0 na posicao do led)
 	ori 	r13, r0, 0xffff
 	orhi	r13, r13, 0xffff
 	xor 	r11, r11, r13
 	
-	# acende o led escolhido
+	# Apaga o led escolhido (desativa o bit correspondente na sequencia de leds)
 	movia 	r12, LED_SEQ
 	ldw 	r13, 0(r12)
 	and 	r13, r13, r11
@@ -364,7 +405,7 @@ LEDS_ON: # comando 02
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
-	# apaga todos os leds
+	# Acende todos os leds
 	movia 	r12, LED_SEQ
 	ori 	r11, r0, 0xFFFF
 	orhi	r11, r11, 0xFFFF
@@ -378,7 +419,7 @@ LEDS_OFF: # comando 03
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
-	# apaga todos os leds
+	# Apaga todos os leds
 	movia 	r12, LED_SEQ
 	stw 	r0, 0(r12)
 	
@@ -392,52 +433,54 @@ SWTCH_NUM: # comando 10
 	
 	call	WORD_OFF # Limpa os displays e seta flag da rotacao pra 0 (comando 21)
 	
-	ldwio	r11, SWTCH(r8)
+	ldwio	r11, SWTCH(r8) # Pega valor das chaves
 	
-	movi	r12, 0x373E # maior valor cujo triangular nao ultrapassa 8 digitos é 14141
-	bge 	r11, r12, DISPLAY_ERROR
+	movi	r12, 0x373E # Maior valor cujo triangular nao ultrapassa 8 digitos é 14141
+	bge 	r11, r12, DISPLAY_ERROR # Caso o valor ultrapasse 14141, mostra erro
 	
 	# Tn = (n.(n+1))/2
 	addi	r12, r11, 0x1
 	mul 	r13, r11, r12
 	movi	r12, 0x2
-	divu	r11, r13, r12
+	divu	r11, r13, r12 # Triangular do valor das chaves
 	
-	and 	r7, r7, r0
-	and 	r6, r6, r0
-	and 	r5, r5, r0
-	GET_DIGIT:
+	and 	r7, r7, r0 # Limpa sequencia dos digitos altos
+	and 	r6, r6, r0 # Limpa sequencia dos digitos baixos
+	and 	r5, r5, r0 # Zera contador
+	GET_DIGIT: # Loop pra pegar a sequencia de digitos do valor calculado
+	/* Pega valor%10 (resto -> digito da unidade), e transforma valor = valor/10 */
 	movi	r12, 0xA
-	divu	r13, r11, r12
-	mul 	r14, r13, r12
+	divu	r13, r11, r12 # valor' = valor/10
+	# Logica para pegar o resto
+	mul 	r14, r13, r12 
 	sub 	r14, r11, r14
-	
+	# Seta valor = valor' (trunca a unidade)
 	mov 	r11, r13
-	
-	movia	r12, D7_SEG
-	add 	r12, r12, r14
-	ldb 	r13, (r12)
+	# Converte valor do digito em código para os displays
+	movia	r12, D7_SEG # Pega endereco da tabela de conversao
+	add 	r12, r12, r14 # Pega da posicao correspondente ao digito (0 a 9)
+	ldb 	r13, (r12) # Pega o codigo da posicao respectiva
 	
 	movi	r12, 0x20
-	bge 	r5, r12, HIGH_DIGIT
-		LOW_DIGIT:
-	sll 	r13, r13, r5
-	or  	r6, r6, r13
+	bge 	r5, r12, HIGH_DIGIT /* Verifica se eh um dos primeiros 4 digitos ou nao */
+		LOW_DIGIT: # Primeiros 4 digitos (digitos baixos)
+	sll 	r13, r13, r5 # Adequa digito a sua posicao respectiva
+	or  	r6, r6, r13 # Adiciona o digito na sequencia
 	br  	END_DIGIT
-		HIGH_DIGIT:
-	subi	r12, r5, 0x20
-	sll 	r13, r13, r12
-	or  	r7, r7, r13
+		HIGH_DIGIT: # Ultimos 4 digitos (digitos altos)
+	subi	r12, r5, 0x20 # Pega posicao normalizada para a parte alta dos displays
+	sll 	r13, r13, r12 # Adequa digito a sua posicao respectiva
+	or  	r7, r7, r13 # Adiciona o digito na sequencia
 		END_DIGIT:
-	addi	r5, r5, 8
-	bne 	r11, r0, GET_DIGIT
-	br  	DISPLAY_NUM
+	addi	r5, r5, 8 # Itera contador (cada digito ocupa 1 byte, portanto)
+	bne 	r11, r0, GET_DIGIT # Se valor' nao for 0, pega proximo digito (unidade do mesmo)
+	br  	DISPLAY_NUM # Caso contrario, finaliza
 	
-		DISPLAY_ERROR:
+		DISPLAY_ERROR: # Caso triangular 9+ digitos, seta valor para a palavra "error"
 	orhi	r6, r0, 0x5000
 	orhi	r7, r0, 0x7950
 	ori 	r7, r7, 0x505C
-		DISPLAY_NUM:
+		DISPLAY_NUM: # Imprime valor nos displays de 7 segmentos
 	stwio	r6, D7S_Low(r8)
 	stwio	r7, D7S_High(r8)
 	
@@ -449,23 +492,22 @@ WORD_ON: # comando 20
 	addi	sp, sp, -4
 	stw 	ra, (sp)
 	
-	ldb 	r11, 0(r9)
-	bne 	r11, r0, SKIP_ROT
-	
+	ldb 	r11, 0(r9) # Verifica a flag de rotacao
+	bne 	r11, r0, SKIP_ROT # Se rotacao ja ligada, pula o comando
+	# Limpa o conteúdo dos displays
 	stwio	r0, D7S_Low(r8)
 	stwio	r0, D7S_High(r8)
-	
-	and 	r11, r11, r0 # limpa tanto parte baixa quanto parte alta
-	
+	/* Imprime a palavra 2021 no centro dos displays [__20 21__] */
+	and 	r11, r11, r0 # Limpa registrador
 	orhi	r11, r0, 0x5B06 # 21 nos displays (parte alta do display baixo)
 	stwio	r11, D7S_Low(r8)
 	
-	and 	r11, r11, r0 # limpa tanto parte baixa quanto parte alta
-	
+	and 	r11, r11, r0 # Limpa registrador
 	ori 	r11, r0, 0x5B3F # 20 nos displays (parte baixa do display alto)
 	stwio	r11, D7S_High(r8)
 	SKIP_ROT:
 	
+	# Seta flag de rotacao para ligada
 	movi	r11, 0x01
 	stb 	r11, 0(r9)
 	
@@ -478,9 +520,11 @@ WORD_OFF: # comando 21
 	stw 	r11, 4(sp)
 	stw 	ra, (sp)
 	
+	# Apaga conteúdo dos displays
 	stwio	r0, D7S_Low(r8)
 	stwio	r0, D7S_High(r8)
 	
+	# Seta flag de rotacao para desligada
 	movi	r11, 0x00
 	stb 	r11, 0(r9)
 	
@@ -489,7 +533,7 @@ WORD_OFF: # comando 21
 	addi	sp, sp, 8
 	ret
 	
-ROTATE_LEFT:
+ROTATE_LEFT: # Roda conteudo dos displays para a esquerda
 	addi	sp, sp, -20
 	stw 	r14, 16(sp)
 	stw 	r13, 12(sp)
@@ -497,20 +541,21 @@ ROTATE_LEFT:
 	stw 	r11, 4(sp)
 	stw 	ra, (sp)
 	
-	ldwio	r11, D7S_Low(r8)
-	andhi	r12, r11, 0xFF00
-	srli	r12, r12, 0x18
+	ldwio	r11, D7S_Low(r8) # Pega valor da parte baixa
+	andhi	r12, r11, 0xFF00 # Salva o ultimo digito da parte baixa
+	srli	r12, r12, 0x18 # Normaliza a posicao do digito para a primeira
 	
-	ldwio	r13, D7S_High(r8)
-	andhi	r14, r13, 0xFF00
-	srli	r14, r14, 0x18
+	ldwio	r13, D7S_High(r8) # Pega valor da parte alta
+	andhi	r14, r13, 0xFF00 # Salva o ultimo digito da parte alta
+	srli	r14, r14, 0x18 # Normaliza a posicao do digito para a primeira
 	
-	slli	r11, r11, 0x8
-	or  	r11, r11, r14
+	slli	r11, r11, 0x8 # Roda a parte baixa em 1 digito (1 byte, 8 bits)
+	or  	r11, r11, r14 # Coloca o digito salvo da parte alta na parte baixa
 	
-	slli	r13, r13, 0x8
-	or  	r13, r13, r12
+	slli	r13, r13, 0x8 # Roda a parte alta em 1 digito (1 byte, 8 bits)
+	or  	r13, r13, r12 # Coloca o digito salvo da parte baixa na parte alta
 	
+	# Atualiza ambas as partes (8 digitos)
 	stwio	r13, D7S_High(r8)
 	stwio	r11, D7S_Low(r8)
 	
@@ -522,7 +567,7 @@ ROTATE_LEFT:
 	addi	sp, sp, 20
 	ret
 	
-ROTATE_RIGHT:
+ROTATE_RIGHT: # Roda conteudo dos displays para a esquerda
 	addi	sp, sp, -20
 	stw 	r14, 16(sp)
 	stw 	r13, 12(sp)
@@ -530,20 +575,21 @@ ROTATE_RIGHT:
 	stw 	r11, 4(sp)
 	stw 	ra, (sp)
 	
-	ldwio	r11, D7S_Low(r8)
-	andi	r12, r11, 0xFF
-	slli	r12, r12, 0x18
+	ldwio	r11, D7S_Low(r8) # Pega valor da parte baixa
+	andi	r12, r11, 0xFF # Salva o primeiro digito da parte baixa
+	slli	r12, r12, 0x18 # Normaliza a posicao do digito para a ultima
 	
-	ldwio	r13, D7S_High(r8)
-	andi	r14, r13, 0xFF
-	slli	r14, r14, 0x18
+	ldwio	r13, D7S_High(r8) # Pega valor da parte alta
+	andi	r14, r13, 0xFF # Salva o primeiro digito da parte alta
+	slli	r14, r14, 0x18 # Normaliza a posicao do digito para a ultima
 	
-	srli	r11, r11, 0x8
-	or  	r11, r11, r14
+	srli	r11, r11, 0x8 # Roda a parte baixa em 1 digito (1 byte, 8 bits)
+	or  	r11, r11, r14 # Coloca o digito salvo da parte alta na parte baixa
 	
-	srli	r13, r13, 0x8
-	or  	r13, r13, r12
+	srli	r13, r13, 0x8 # Roda a parte alta em 1 digito (1 byte, 8 bits)
+	or  	r13, r13, r12 # Coloca o digito salvo da parte baixa na parte alta
 	
+	# Atualiza ambas as partes (8 digitos)
 	stwio	r13, D7S_High(r8)
 	stwio	r11, D7S_Low(r8)
 	
@@ -612,19 +658,19 @@ MAIN:
 	WAIT:
 	subi	r11, r11, 0x1
 	bne 	r0, r11, WAIT
-	
+	# Verifica se a rotacao esta ligada ou desligada
 	ldb 	r13, 0(r9)
-	beq 	r13, r0, END_ROT
-	
+	beq 	r13, r0, END_ROT # Se == 0 (desligada), pula pro final
+	# Verifica se a rotacao esta pausada ou despausada
 	movi	r11, 0x1
 	ldb 	r13, 1(r9)
-	beq 	r13, r0, END_ROT
-	beq 	r13, r11, R_ROT
+	beq 	r13, r0, END_ROT # Se == 0 (pausada), pula pro final
+	beq 	r13, r11, R_ROT # Se == 1 (esquerda), pula pra rotacao direita
 	L_ROT:
-	call	ROTATE_LEFT
+	call	ROTATE_LEFT # Chama se 1(r9) == 2
 	br	END_ROT
 	R_ROT:
-	call	ROTATE_RIGHT
+	call	ROTATE_RIGHT # Chama se 1(r9) == 1
 	END_ROT:
 	
 	br	MAIN
